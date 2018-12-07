57bb4244ce52769e41335d1d751cf95b2c2bc52f
Andrei Cacio <andrei.cacio@yahoo.com>
2018-12-07 08:53:10 +0200

    Started on day2

---
+++ day2/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+fn main() {
+	println!("--== Advent of Code 2018 ==--"); 
+
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let db: Vec<&str> = contents.split_terminator('\n').collect();
+    let mut letters = HashMap::new();
+
+    db.iter().for_each(|id| {
+        for ch in id.chars().into_iter() {
+            print!("{} ", ch);
+        }
+
+        println!("");
+    });
+
+    // println!("\nDay1 result: {:?}", res); 
+}
