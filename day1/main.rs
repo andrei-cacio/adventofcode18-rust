6fe5e1dce35aa5ee51233d12288d0b80becc3b37
andrei-cacio <andrei@moqups.com>
2018-12-19 12:11:13 +0200

    Finished day1

--- day1/main.rs
+++ day1/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
+
 
 fn main() {
 	println!("--== Advent of Code 2018 ==--"); 
@@ -14,7 +16,35 @@ fn main() {
         .expect("something went wrong reading the file");
 
     let v: Vec<&str> = contents.split_terminator('\n').collect();
-    let res: i32 = v.iter().fold(0, |acc, x| acc + x.parse::<i32>().unwrap());
+    let nr_v: Vec<i32> = v.iter().map(|nr| nr.parse::<i32>().unwrap()).collect();
+    let mut freqs_map = HashMap::new();
 
-    println!("\nDay1 result: {:?}", res); 
+    let mut first_repeat_freq = 0;
+    let mut first_freq = 0;
+    let mut last_freq = 0;
+    let mut iterations = 0;
+
+    while first_repeat_freq == 0 {
+        last_freq = nr_v.iter().fold(last_freq, |acc, x| {
+            let freq = acc + x;
+            
+            if freqs_map.contains_key(&freq) && first_repeat_freq == 0 {
+                first_repeat_freq = freq;
+            } else {
+                freqs_map.insert(freq, freq);    
+            }
+
+            return freq;
+        });
+
+        if first_freq == 0 {
+            first_freq = last_freq;
+        }
+
+        iterations += 1;    
+    }
+    
+
+    println!("\nDay1 1/2 result: {:?}", first_freq); 
+    println!("Day1 2/2 result: {} [iterations: {}]", first_repeat_freq, iterations);
 }
