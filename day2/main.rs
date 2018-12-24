daba83617eb44576a1a5622666ce09f7eacc2a92
Andrei Cacio <andrei.cacio@yahoo.com>
2018-12-24 15:21:57 +0200

    Day 2 first problem down

--- day2/main.rs
+++ day2/main.rs
@@ -15,18 +15,31 @@ fn main() {
         .expect("something went wrong reading the file");
 
     let db: Vec<&str> = contents.split_terminator('\n').collect();
-    let mut letters = HashMap::new();
+    let mut counts2_map: HashMap<&str, i32> = HashMap::new();
+    let mut counts3_map: HashMap<&str, i32> = HashMap::new();
 
     db.iter().for_each(|id| {
+        let mut letters: HashMap<char, i32> = HashMap::new();
         for ch in id.chars() {
+            let mut new_freq;
             match letters.get(&ch) {
-                Some(mut x) => letters.insert(ch, x+1),
-                None => letters.insert(ch, 1),
+                Some(mut old_freq) => new_freq = old_freq + 1,
+                None => new_freq = 1,
             };
+            letters.insert(ch, new_freq);
+        }
+        
+
+        for key in letters.keys() {
+            let freq: i32 = *letters.get(&key).unwrap();
+            if freq == 2 {
+                counts2_map.insert(id, 2);
+            }
+            if freq == 3 {
+                counts3_map.insert(id, 3);
+            }
         }
     });
 
-    println!("{:?}", letters);
-
-    // println!("\nDay1 result: {:?}", res); 
+    println!("\nDay2.1 result: {:?}", counts2_map.keys().count() * counts3_map.keys().count()); 
 }
